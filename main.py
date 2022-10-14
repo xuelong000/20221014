d9fd7b65eac9fe9f9acc9587724f068795b3bd07
xuelong000 <115683800+xuelong000@users.noreply.github.com>
2022-10-14 20:27:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Weather:
     def __init__(self):
         self.config = ConfigParser()
         try:
-            self.config_path = os.getcwd() + r'\config.ini'
+            self.config_path = os.getcwd() + r'\config.txt'
             self.config.read(self.config_path, encoding='utf-8')
         except:
             print('读取配置文件出错，请检查文件格式')
